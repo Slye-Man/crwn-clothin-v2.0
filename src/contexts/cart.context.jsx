4ad034b6a836b3d9507835a3641e58ba6cd7deab
Selaelo Sebola <selaelosebola67@gmail.com>
2023-02-01 23:58:14 +0200

    implemented remove from cart functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -17,11 +17,29 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }]
 }
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map((cartItem) => 
+        cartItem.id === cartItemToRemove.id ?
+        {...cartItem, quantity: cartItem.quantity -1 } :
+        cartItem
+    )
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    removeItemFromCart: () => {},
     cartItemCount: 0
 });
 
@@ -40,7 +58,11 @@ export const CartProvider = ({ children }) => {
         setCartItems(addCartItem(cartItems, productToAdd))
     }
 
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartItemCount };
+    const removeItemFromCart = (cartItemToRemove) => {
+        setCartItems(addCartItem(cartItems, cartItemToRemove))
+    }
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartItemCount, removeItemFromCart };
     return <CartContext.Provider value={value}>
         {children}
     </CartContext.Provider>;

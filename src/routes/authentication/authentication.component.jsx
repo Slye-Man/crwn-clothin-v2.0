d71cac4264ac8a306373659d6b7f9f1a041e243b
Selaelo Sebola <selaelosebola67@gmail.com>
2023-02-06 22:28:21 +0200

    fixed spelling error

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -5,7 +5,7 @@ import './authentication.styles.scss'
 
 const Authentication = () => {
     return (
-        <div className="authentication-con">
+        <div className="authentication-container">
             <SignInForm />
             <SignUpForm />
         </div>

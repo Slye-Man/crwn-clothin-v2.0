018dd92c8721800add29343de93a84ad0b94a858
Selaelo Sebola <selaelosebola67@gmail.com>
2023-02-06 22:29:41 +0200

    updating shop_data

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
 
-import PRODUCTS from '../shop-data.json'
+import SHOP_DATA from "../shop-data";
 
 export const ProductsContext = createContext({
     products: []
 })
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS)
+    const [products, setProducts] = useState([])
+    // setting the values once. Needs to be deleted once values are set
+    // In my case I'm commenting the useEffect because I would like to
+    // change the values later on
+    // useEffect(() => {
+    //     addCollectionAndDocuments('categories', SHOP_DATA)
+    // }, [])
     const value = {products}
     return (
         <ProductsContext.Provider value={value}>
